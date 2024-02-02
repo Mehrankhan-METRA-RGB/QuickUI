c7ef8f7de77418f54edec35ed26ae5615eeef40e
Mehran <m.jan9396@hotmail.com>
2024-02-03 01:06:15 +0500

    Quick Router implemented

--- quick_ui/lib/quick_ui.dart
+++ quick_ui/lib/quick_ui.dart
@@ -1,7 +1,11 @@
 library quick_ui;
 
+export 'Test/stop_watch.dart';
+export 'Utils/Routers/quick_routers.dart';
 export 'Widgets/Bars/simple_bottom_bar.dart';
 export 'Widgets/Buttons/quick_float.dart';
 export 'Widgets/Chips/quick_chip.dart';
+export 'Widgets/Contexts/Toast/toast.dart';
+export 'Widgets/Cropper/quick_crop.dart';
 export 'Widgets/Images/quick_image.dart';
 export 'Widgets/InputFields/input_field.dart';
